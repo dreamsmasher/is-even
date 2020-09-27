fbdcb2cc87bfc58d93dc1396c1c2e8c4ecd14955
Norman Liu <normanwuweiliu@gmail.com>
2020-09-27 14:02:36 -0700

    Added tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,15 @@
 import Test.QuickCheck
 import Data.Numbers.IsEven
 
+propEven :: [Int] -> Bool
+propEven xs = map isEven xs == map even xs
+
+
+propOdd :: [Int] -> Bool
+propOdd xs = map isOdd xs == map odd xs
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    verboseCheck propEven
+    verboseCheck propOdd
