5dc0883087e813df7e378e2121825644e70f3aad
Naomi Liu <naomi@nliu.net>
2020-10-15 12:21:10 -0700

    modified tests and build flags (-O2)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,7 @@
 import Test.QuickCheck
+import Test.Hspec
+import Test.Hspec.Runner
+import Test.Hspec.QuickCheck
 import Data.Numbers.IsEven
 
 propEven :: [Int] -> Bool
@@ -9,7 +12,18 @@ propOdd :: [Int] -> Bool
 propOdd xs = map isOdd xs == map odd xs
 
 
+spec :: Spec
+spec = do
+    modifyMaxSuccess (const 1000) $ it "checks if a number is even" $ property propEven
+    modifyMaxSuccess (const 1000) $ it "checks if a number is odd" $ property propOdd
+
+config :: Config
+config = defaultConfig {
+        configPrintCpuTime = True,
+        configRandomize = True,
+        configColorMode = ColorAuto
+
+                       }
 main :: IO ()
-main = do
-    verboseCheck propEven
-    verboseCheck propOdd
+main = readConfig config [] >>= runSpec spec >>= evaluateSummary
+
